d2604ed019f05501a1aa6d970ab4280bf068548c
Izzy-glen <takorglen@gmail.com>
2019-08-12 15:32:04 +0100

    3rd commit with new landing and edited cards

--- js/script.js
+++ js/script.js
@@ -1,4 +1,13 @@
-// Goodbye modal
-$(window).on("beforeunload", function() {
-  $(#modalPoll_1).modal('show');
-});
+// Changing lading pages based on screen resize
+var browserWidth = $(window).width();
+var documentHeight = $(window).height();
+    $(".banner").height(documentHeight);
+setInterval(function () {
+  $(window).on('resize', function (){
+var documentHeight = $(window).height();
+    $(".banner").height(documentHeight);
+  });
+// if (browserWidth > 767) {
+//   $(".banner").css({"background":"url(../stock/bg2.jpg)"});
+// }
+}, 5000);
